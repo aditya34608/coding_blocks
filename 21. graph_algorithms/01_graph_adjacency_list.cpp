1b310d672e6ba051e8b9d43876b70eb8dabce3b6
Aditya Kumar <38347787+aditya34608@users.noreply.github.com>
2020-07-07 15:13:03 +0530

    Add files via upload

---
+++ 21. graph_algorithms/01_graph_adjacency_list.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+class Graph
+{
+    int v;
+
+    //array of list
+    list<int> *l;
+
+public:
+    Graph(int v)
+    {
+        this->v = v;
+        l = new list<int>[v];
+    }
+
+    void addEdge(int x, int y)
+    {
+        l[x].push_back(y);
+        l[y].push_back(x);
+    }
+
+    void printAdjList()
+    {
+        //iterate over all the vertices
+        for (int i = 0; i < v; i++)
+        {
+            cout << "Vertex " << i << "->";
+            for (int nbr : l[i])
+            {
+                cout << nbr << ",";
+            }
+            cout << "\n";
+        }
+    }
+};
+
+int main()
+{
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(2, 3);
+    g.addEdge(1, 2);
+    g.printAdjList();
+
+    return 0;
+}
